68f32f39c5f984fcf545f8662c20bc162da1f637
Bui Manh Tien <tienbm.ho@mcredit.com.vn>
2022-11-20 15:38:42 +0700

    update: provider config

--- mysql/main.tf
+++ mysql/main.tf
@@ -9,13 +9,13 @@
 terraform {
   # Live modules pin exact Terraform version; generic modules let consumers pin the version.
   # The latest version of Terragrunt (v0.36.0 and above) recommends Terraform 1.1.4 or above.
-  required_version = "= 1.1.4"
+  # required_version = "= 1.1.4"
 
   # Live modules pin exact provider version; generic modules let consumers pin the version.
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 3.7.0"
+      version = "~> 4.21.0"
     }
   }
 }
